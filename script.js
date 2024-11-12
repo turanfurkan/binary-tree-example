d9d15e727eae1421733fbfcc0a91ced2d2411db3
Furkanturanr <furkanturan10r@hotmail.com>
2024-11-12 17:42:07 +0300

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,79 @@
-window.onload = function(){
-    loadNotes();
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
 }
 
-function loadNotes()
-{
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
 
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    inOrder(node = this.root) {
+        if (node !== null) {
+            this.inOrder(node.left);
+            console.log(node.value);
+            this.inOrder(node.right);
+        }
+    }
+
+    preOrder(node = this.root) {
+        if (node !== null) {
+            console.log(node.value);
+            this.preOrder(node.left);
+            this.preOrder(node.right);
+        }
+    }
+
+    postOrder(node = this.root) {
+        if (node !== null) {
+            this.postOrder(node.left);
+            this.postOrder(node.right);
+            console.log(node.value);
+        }
+    }
 }
+
+// Kullanım
+const tree = new BinaryTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(3);
+tree.insert(7);
+
+console.log("Inorder Traversal:");
+tree.inOrder();
+
+console.log("Preorder Traversal:");
+tree.preOrder();
+
+console.log("Postorder Traversal:");
+tree.postOrder();
